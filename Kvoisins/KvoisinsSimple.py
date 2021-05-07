53bd60ffad38c5b80be1edef2506f9ded66753fe
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-05-07 21:30:28 +0200

    PEP

--- Kvoisins/KvoisinsSimple.py
+++ Kvoisins/KvoisinsSimple.py
@@ -3,7 +3,8 @@ Implémentation simple (2D/carac) d'un algo des k plus proches voisins
 """
 from math import sqrt
 
-def KVoisins(ano, database:list[list], K=3, typeindex=0):
+
+def KVoisins(ano, database: list[list], K=3, typeindex=0):
     """
     Donne le type de la nouvelle valeur selon les données preétablies
 
@@ -25,7 +26,9 @@ def KVoisins(ano, database:list[list], K=3, typeindex=0):
             - prob : probabilité que le type identifié soit correct (en %)
     """
     iD = ""
-    distances = [[item[typeindex],sqrt(sum([(ano[i]-float(item[i]))**2 for i in range(len(ano)) if i!=typeindex]))] for item in database]
+    distances = [[item[typeindex], sqrt(sum(
+        [(ano[i]-float(item[i]))**2 for i in range(len(ano)) if i != typeindex]))] 
+            for item in database]
     kvList = sorted(distances)[:K]
     dictt = {}
     typeList = list(set([kv[0] for kv in kvList]))
@@ -33,29 +36,29 @@ def KVoisins(ano, database:list[list], K=3, typeindex=0):
     tot = 0
     print(typeList)
     for t in typeList:
-        v = len([i for i in kvList if i[0]==t])
+        v = len([i for i in kvList if i[0] == t])
         dictt[t] = v
         tot += v
-        if dictt[t]>vmax:
+        if dictt[t] > vmax:
             iD = t
             vmax = dictt[t]
-    prob = round(vmax/tot*100,3)
+    prob = round(vmax/tot*100, 3)
 
-    return (iD,prob)
+    return (iD, prob)
 
 
-if __name__=='__main__': # test
+if __name__ == '__main__':  # test
     db = [
-        ["H",178,72],
-        ["H",174,72],
-        ["F",163,55],
-        ["F",168,58],
-        ["H",181,98],
-        ["F",170,60],
-        ["H",184,78],
-        ["F",171,59]
+        ["H", 178, 72],
+        ["H", 174, 72],
+        ["F", 163, 55],
+        ["F", 168, 58],
+        ["H", 181, 98],
+        ["F", 170, 60],
+        ["H", 184, 78],
+        ["F", 171, 59]
     ]
-    ano = [169,65]
+    ano = [169, 65]
     retval = KVoisins(ano, db)
     (iD, prob) = retval
     print(f"type de donnée : {iD}")
